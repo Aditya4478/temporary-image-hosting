d77e2b8526485de29ff9869245d507d09ecdc071
Aditya Shantilal Bathani <95112738+Aditya4478@users.noreply.github.com>
2023-12-02 18:44:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let flagFound = 0;
+if (typeof flagFound === 'undefined') {
+    var flagFound = 0;
+}
 // Code 1: Selecting a specific option in the dropdown
 var dropdown = document.getElementById("post_select");
 var desiredOptionValue = "486bf614-b0db-ec11-a7b4-001dd80234f6";
@@ -34,7 +36,9 @@ setTimeout(function () {
                     audio.play();
                     clickCounter = 100; // preventing the calendarContainer to get closed
                     clearInterval(intervalId); // Ending Loop
-                    flagFound = 1;
+                    if (flagFound = 0) {
+                        flagFound = 1;
+                    }
 
                     var greenDayCell = document.querySelector('td.greenday');
 
